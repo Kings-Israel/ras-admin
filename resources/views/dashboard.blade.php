60951dc9f3011ac7a4375eeab6c0226456d7a984
steven-maina <stevenmaina17@gmail.com>
2023-12-04 09:04:07 +0300

    financier debugs

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -228,8 +228,8 @@
 
                         <div class="body" id="financing-limit" style="display: block">
                             <span class="font-bold">Financing Limit</span>
-                            <h3 class="m-b-10 number count-to" data-from="0" data-to="{{ number_format(floatval($financing_total_limit), 2) }}"
-                                data-speed="100" data-fresh-interval="200">"{{ number_format(floatval($financing_total_limit), 2) }}"
+                            <h3 class="m-b-10 number count-to" data-from="0" data-to="{{ $financing_total_limit}}"
+                                data-speed="100" data-fresh-interval="200">"{{ $financing_total_limit }}"
                             </h3>
                         </div>
                 </div>
@@ -247,8 +247,8 @@
                         </div>
                         <div class="body" id="financing-disbursed" style="display: block">
                             <span class="font-bold">Disbursed Amount</span>
-                            <h3 class="m-b-10 number count-to" data-from="0" data-to="{{ number_format(floatval($financing_total_invoices), 2) }}"
-                                data-speed="100" data-fresh-interval="200">"{{ number_format(floatval($financing_total_invoices), 2) }}"
+                            <h3 class="m-b-10 number count-to" data-from="0" data-to="{{$financing_total_invoices}}"
+                                data-speed="100" data-fresh-interval="200">"{{$financing_total_invoices}}"
                             </h3>
                         </div>
                 </div>
